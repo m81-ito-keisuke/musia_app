2664941031d71ffa2dc869e540ce1699da593f0d
sia-yuki-f717 <larc_bump717kei@yahoo.co.jp>
2021-01-22 07:40:56 +0900

    データベースの表示不具合調整

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,6 @@ class UsersController < ApplicationController
   def show
     user = User.find(params[:id])
     @nickname = user.nickname
-    @tweets = user.tweets
-    @tweets = Tweet.includes(:user).order("created_at DESC")
+    @tweets = user.tweets.order("created_at DESC")
   end
 end
