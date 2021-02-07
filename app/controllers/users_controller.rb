5029996bbc096660bc90d6f8b876bcae3f2d3f3f
sia-yuki-f717 <larc_bump717kei@yahoo.co.jp>
2021-02-08 01:06:46 +0900

    before_actionの設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,29 @@
 class UsersController < ApplicationController
+before_action :set_user
+
   def show
-    @user = User.find(params[:id])
     @nickname = @user.nickname
     @musics = @user.musics.order('created_at DESC')
     @favorite_musics = @user.favorite_musics
   end
 
   def favorites
-    @user = User.find(params[:id])
     @nickname = @user.nickname
     @musics = @user.musics.order('created_at DESC')
     @favorite_musics = @user.favorite_musics
   end
 
   def following
-    @user = User.find(params[:id])
     @users = @user.followings
   end
 
   def followers
-    @user  = User.find(params[:id])
     @users = @user.followers
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
