c98c607ecd90860009d871dee0c08cfbdccfd865
sia-yuki-f717 <larc_bump717kei@yahoo.co.jp>
2021-03-09 16:34:40 +0900

    リダイレクトの設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,11 @@
 class OrdersController < ApplicationController
 
   def new
+    @music = Music.find(params[:music_id])
     @order = Order.new
+    if current_user.id == @music.user_id
+      redirect_to root_path
+    end
   end
 
   def create
