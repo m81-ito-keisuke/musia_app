38ab0886a58cb5d6be73db09c9a4b8ff3b12b4db
伊藤　圭祐 <m81.itou.keisuke@gmail.com>
2022-01-25 20:00:21 +0900

    recommit2

--- app/helpers/musics_helper.rb
+++ app/helpers/musics_helper.rb
@@ -1,7 +1,7 @@
 module MusicsHelper
   def music_lists(musics)
     html = ''
-    musics.each do |music|
+    @musics.each do |music|
       html += render(partial: 'music', locals: { music: music })
     end
     raw(html)
