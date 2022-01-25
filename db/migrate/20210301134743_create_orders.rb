176a376b7429d838b4bc82201032b04ef846082e
伊藤　圭祐 <m81.itou.keisuke@gmail.com>
2022-01-25 22:34:05 +0900

    recommit7

--- db/migrate/20210301134743_create_orders.rb
+++ db/migrate/20210301134743_create_orders.rb
@@ -1,8 +1,8 @@
 class CreateOrders < ActiveRecord::Migration[6.0]
   def change
     create_table :orders do |t|
-      t.integer :music_id  ,null: false
-      t.integer :user_id  ,null: false
+      t.integer :music_id  ,null: false, foreign_key: true
+      t.integer :user_id  ,null: false, foreign_key: true
       t.timestamps
     end
   end
